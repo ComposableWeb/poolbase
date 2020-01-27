aebe05f3e9a1ffe9a7bff85bd8cb9af97c7da174
Andreas Sahle <andreas.sahle@gmail.com>
2020-01-27 18:03:06 +0100

    fix: typescript config

--- packages/browser-extension/src/background.ts
+++ packages/browser-extension/src/background.ts
@@ -1,4 +1,4 @@
-async function savePage<T>(url: string, title: string): Promise<T> {
+async function savePage(url: string, title: string) {
   return fetch('https://us-central1-poolbase-123.cloudfunctions.net/savePage', {
     method: 'POST',
     mode: 'cors',
@@ -12,20 +12,20 @@ async function savePage<T>(url: string, title: string): Promise<T> {
       title,
     }),
   }).then(
-    (response): Promise<T> => {
+    (response) => {
       if (!response.ok) {
         throw new Error(response.statusText);
       }
-      return response.json() as Promise<T>;
+      return response.json();
     }
   );
 }
 
 chrome.browserAction.onClicked.addListener(function(tab): void {
   // Send a message to the active tab
-  chrome.tabs.query({ active: true, currentWindow: true }, async function(tabs): void {
+  chrome.tabs.query({ active: true, currentWindow: true }, async function(tabs) {
     const activeTab = tabs[0];
-    const response = await savePage<{}>(activeTab.url, activeTab.title);
+    const response = await savePage(activeTab.url, activeTab.title);
     console.log(response);
     chrome.tabs.sendMessage(activeTab.id, {
       message: 'clicked_browser_action',
