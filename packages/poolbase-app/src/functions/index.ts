8b45bbb51de64175c38e84fa162d2ea175428e56
Andreas Sahle <andreas.sahle@gmail.com>
2020-01-28 12:11:26 +0100

    chore: todos for save function

--- packages/poolbase-app/src/functions/index.ts
+++ packages/poolbase-app/src/functions/index.ts
@@ -1,12 +1,21 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import next from 'next'
-
+/* TODO: refactor and improve
+- [ ] put firebase admin initialization in singleton module
+- [ ] create request validation function
+- [ ] create data validation function
+- [ ] create save function that is backend agnostic
+*/
 admin.initializeApp();
 
-export const savePage = functions.https.onRequest(async (request, response) => {
+export const savePage = functions.https.onRequest(async (req, res) => {
+  // minimal requirement is URL
+  if(!req.body.url) {
+    res.status(400).send('No url data provided');
+  }
   const requestData: { title: string; url: string } = {
-    ...request.body,
+    ...req.body,
   };
   // Push the new message into Cloud Firestore using the Firebase Admin SDK.
   const writeResult = await admin
@@ -14,7 +23,7 @@ export const savePage = functions.https.onRequest(async (request, response) => {
     .collection('pages')
     .add(requestData);
   // Send back a message that we've successfully written the message
-  response.json({ result: `Page with url: ${writeResult.id} added.` });
+  res.json({ result: `Page with url: ${writeResult.id} added.` });
 });
 
 const dev = process.env.NODE_ENV !== 'production';
