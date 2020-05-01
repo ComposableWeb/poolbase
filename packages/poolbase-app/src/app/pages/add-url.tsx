67c4e58bd2f61e468b103c827f55ab866929c4eb
Andreas Sahle <andreas.sahle@gmail.com>
2020-05-01 19:55:56 +0200

    chore: use collection data directly

--- packages/poolbase-app/src/app/pages/add-url.tsx
+++ packages/poolbase-app/src/app/pages/add-url.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import { NextPage } from 'next';
-import { useCollection } from 'react-firebase-hooks/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import { ListItem } from '@poolbase/design-system/src/components/ListItem';
 import { PageData } from '@poolbase/common';
@@ -12,14 +12,14 @@ import { AddUrlForm } from '../components/AddUrlForm';
 
 const AddUrlPage: NextPage = () => {
   const query = firestore.collection('pages').orderBy('created', 'desc').limit(3);
-  const [data, loading, error] = useCollection(query);
+  const [data, loading, error] = useCollectionData<PageData>(query, { idField: 'id' });
 
   return (
     <PageLayout>
       <AddUrlForm />
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && <span>Collection: Loading...</span>}
-      {data && data.docs.map((page): JSX.Element => <ListItem key={page.id} data={page.data() as PageData} />)}
+      {data && data.map((page): JSX.Element => <ListItem key={page.id} data={page} />)}
     </PageLayout>
   );
 };
--- packages/poolbase-app/src/app/pages/index.tsx
+++ packages/poolbase-app/src/app/pages/index.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import { NextPage } from 'next';
-import { useCollection } from 'react-firebase-hooks/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import { ListItem } from '@poolbase/design-system/src/components/ListItem';
 import { PageData } from '@poolbase/common';
@@ -17,14 +17,14 @@ interface HomePageProps {
 
 const HomePage: NextPage<HomePageProps> = ({ t }: HomePageProps) => {
   const query = firestore.collection('pages').orderBy('created', 'desc').limit(30);
-  const [data, loading, error] = useCollection(query);
+  const [data, loading, error] = useCollectionData<PageData>(query, { idField: 'id' });
 
   return (
     <PageLayout>
       <h1>{t('siteTitle')}</h1>
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && <span>Collection: Loading...</span>}
-      {data && data.docs.map((page): JSX.Element => <ListItem key={page.id} data={page.data() as PageData} />)}
+      {data && data.map((page): JSX.Element => <ListItem key={page.id} data={page} />)}
     </PageLayout>
   );
 };
