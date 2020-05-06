b775e8f6d6ab4eab1ea86e6aef59b7cc4456001c
Andreas Sahle <andreas.sahle@gmail.com>
2020-05-06 14:07:09 +0200

    chore: remove unused code

--- packages/poolbase-app/src/app/pages/api/share-url.ts
+++ packages/poolbase-app/src/app/pages/api/share-url.ts
@@ -1,28 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import formidable from 'formidable';
 import { auth } from 'firebase-admin';
-import admin, { firestore } from '../../../functions/initFirebaseAdmin';
 
 // @TODO: refactor to use cloud function
-const handler = async (
+const handler = (
   req: NextApiRequest & { session?: { decodedToken: auth.DecodedIdToken; token: string } },
   res: NextApiResponse
-): Promise<void> => {
-  const form = formidable();
-  await form.parse(req, async (err, fields) => {
-    if (err) {
-      return;
-    }
-    const urlData = {
-      ...fields,
-      created: admin.firestore.FieldValue.serverTimestamp(),
-      status: 'new',
-      processed: {},
-    };
-    // Push the new url into Cloud Firestore using the Firebase Admin SDK.
-    console.info(urlData, req.session);
-    return await firestore.collection('pages').add(urlData);
-  });
+): void => {
+  // @TODO: forward the post data to callable firebase function
 
   res.end();
 };
